66a5cba5c84188e221a5bc414d224e14085a100f
XGWSDFE <david.feldhoff@web.de>
2017-03-10 19:16:00 +0100

    register.jsf steht

--- Eventon/src/main/java/de/eventon/core/Address.java
+++ Eventon/src/main/java/de/eventon/core/Address.java
@@ -3,32 +3,32 @@ package de.eventon.core;
 public class Address {
 
 	private String locationName;
-	private String plz;
+	private String zip;
 	private String city;
 	private String street;
 	private int streetnumber;
 	
-	public Address(String plz, String city, String street, int streetnumber) {
-		this.plz = plz;
+	public Address(String zip, String city, String street, int streetnumber) {
+		this.zip = zip;
 		this.city = city;
 		this.street = street;
 		this.streetnumber = streetnumber;
 	}
 	
-	public Address(String locationName, String plz, String city, String street, int streetnumber) {
+	public Address(String locationName, String zip, String city, String street, int streetnumber) {
 		this.locationName = locationName;
-		this.plz = plz;
+		this.zip = zip;
 		this.city = city;
 		this.street = street;
 		this.streetnumber = streetnumber;
 	}
 	
-	public String getPlz() {
-		return plz;
+	public String getZIP() {
+		return zip;
 	}
 
-	public void setPlz(String plz) {
-		this.plz = plz;
+	public void setZIP(String plz) {
+		this.zip = plz;
 	}
 
 	public String getCity() {

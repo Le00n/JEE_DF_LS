3cff9f23599cc52e1c65ddf4f1fcce2f7d35bda5
xgadles <leonstapper@gmx.de>
2017-03-30 01:05:44 +0200

    Data

---
+++ Eventon/src/main/java/de/eventon/services/impl/DatabaseConfigurationService.java
@@ -0,0 +1,22 @@
+package de.eventon.services.impl;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+@ApplicationScoped
+public class DatabaseConfigurationService implements Serializable{
+
+	private static final long serialVersionUID = -2267992486213377214L;
+
+	@Produces
+	@ApplicationScoped
+	EntityManager createEntityManager() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("h2");
+		return emf.createEntityManager();
+	}
+}
